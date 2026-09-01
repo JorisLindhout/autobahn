4e928e7b686d75b54a55a1c7bb00c1d4dc27f562
Joris Lindhout <jorislindhout@gmail.com>
2026-09-01 08:40:41 -0500

    Change port for local dev

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 5174,
+    strictPort: true,
     allowedHosts: ['.trycloudflare.com'],
   },
   build: {
